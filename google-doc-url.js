9c04bb20edf2cd34c829f8e48a3c89aa0e281e05
Elisa <elisabrigettecastro@gmail.com>
2026-04-15 11:55:17 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Vnwg_XQ6LsaxiOukTuizGulXC7TDPoX8in9BrN-CQp0/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/15Tq4O8RQmSvOBmt3djDJDPLdJIUm0GGvNLRGroukPuI/edit?gid=858846768#gid=858846768';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
